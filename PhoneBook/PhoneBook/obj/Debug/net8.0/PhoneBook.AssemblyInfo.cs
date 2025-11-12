ffa4cb972e30b4b4b261600443ed6126865a768a
Phust552002 <huynhthanhphuc2002@gmal.com>
2025-11-12 07:54:34 +0700

    SP-Fix2

--- PhoneBook/PhoneBook/obj/Debug/net8.0/PhoneBook.AssemblyInfo.cs
+++ PhoneBook/PhoneBook/obj/Debug/net8.0/PhoneBook.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PhoneBook")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a48aeda9742e935f2b2970de4879a3636d214bda")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0dd0f8fa063a4e72b971723d4304d7de4f89a6f1")]
 [assembly: System.Reflection.AssemblyProductAttribute("PhoneBook")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PhoneBook")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
