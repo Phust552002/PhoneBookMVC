184dbe23d85628100a2d056872c48c954b23a9d3
Phust552002 <huynhthanhphuc2002@gmal.com>
2025-11-13 08:57:39 +0700

    Css_change theme color and add resx for login

--- PhoneBook/PhoneBook/obj/Debug/net8.0/PhoneBook.AssemblyInfo.cs
+++ PhoneBook/PhoneBook/obj/Debug/net8.0/PhoneBook.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PhoneBook")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a007cdff63e5ac050c809f5d68d49ec7c5ff8680")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4799f1db36b71a518d8a6012c818b7e20128f75b")]
 [assembly: System.Reflection.AssemblyProductAttribute("PhoneBook")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PhoneBook")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- PhoneBook/PhoneBook/obj/Release/net8.0/PhoneBook.AssemblyInfo.cs
+++ PhoneBook/PhoneBook/obj/Release/net8.0/PhoneBook.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PhoneBook")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ac20b8b4837bd60478bd4529146934d84b34e5cc")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4799f1db36b71a518d8a6012c818b7e20128f75b")]
 [assembly: System.Reflection.AssemblyProductAttribute("PhoneBook")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PhoneBook")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
